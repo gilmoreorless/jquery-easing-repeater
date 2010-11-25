6dad882037f323b69aa8d03783302852e339e358
Gilmore Davidson <gilmoreorless@gmail.com>
2010-11-26 07:47:43 +1100

    Added copyright and minified version

--- jquery.easing.repeater.js
+++ jquery.easing.repeater.js
@@ -1,7 +1,15 @@
 /*!
  * jQuery Easing Repeater plugin v0.1
+ * Copyright (c) 2010 Gilmore Davidson
+ * https://github.com/gilmoreorless/jquery-easing-repeater
+ */
+/**
+ * @requires jQuery v1.2 or later
  *
  * Repeats a given jQuery easing method several times over the course of a single animation
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
  */
 ;(function ($) {
 	/**
